e45b616aa0405738077c5647edf46fa776c3e0dc
luminiksik@yandex.ru <Qfyfcnr94>
2016-11-19 09:54:47 +0300

    add method "seach all"
    Добавляем метод для просмотра каталога и счёта всез расширений в ней

--- src/TheBypassFiles.java
+++ src/TheBypassFiles.java
@@ -1,20 +1,34 @@
 import java.io.File;
 import java.io.FilenameFilter;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Liminiksik on 05.11.2016.
  */
 public class TheBypassFiles {
         public static int k = 0;
+        private static Map<String, Integer> count = new HashMap<String, Integer>();
 
         public static void main(String[] args) {
             // определяем объект для каталога
-            File dir = new File("C:\\work\\Мамке");
+            //File dir = new File("D:\\new\\work\\Мамке"); //D:\new\work\Мамке
+            File dir = new File("D:\\new\\Новая папка (2)\\Pictures"); //D:\new\Новая папка (2)\Pictures
+
             String str = ".doc";
             filter(dir, str);
             // если объект представляет каталог
             int c = folder(dir);
             System.out.println("колличество файлов  " + c);
+
+            System.out.println();
+            System.out.println("--------------------------------------------------");
+            filter1(dir);
+            for (String str1: count.keySet()) {
+                if (str1.equals(" "))
+                    System.out.println("Файлов без расширения - " + count.get(str1) + " штук");
+                else System.out.println("Файлов с расширением " + str1 + " - " + count.get(str1) + " штук");
+            }
 
         }
 
@@ -29,6 +43,41 @@ public class TheBypassFiles {
             for(File f : listFiles)
                 System.out.println("Файл: " + dir + File.separator + f.getName());
         }
+    }
+
+    public static void filter1 (File dir){
+        if(!dir.exists())
+            System.out.println(dir + " папки не существует");
+        if(dir.isDirectory()){
+            // получаем все вложенные объекты в каталоге
+            File[] listFiles;
+            for(File item : dir.listFiles()){
+                if(item.isDirectory()) {
+                    filter1(item);
+                } else {
+                    String str = getFileExtension(item);
+                    if (count.containsKey(str)){
+                        Integer n = new Integer(count.get(str));
+                        n++;
+                        count.put(str, n);
+                    }
+                    else
+                        count.put(str, new Integer(1));
+
+                }
+            }
+        }
+    }
+
+    //метод определения расширения файла
+    private static String getFileExtension(File file) {
+        String fileName = file.getName();
+        // если в имени файла есть точка и она не является первым символом в названии файла
+        if(fileName.lastIndexOf(".") != -1 && fileName.lastIndexOf(".") != 0)
+            // то вырезаем все знаки после последней точки в названии файла, то есть ХХХХХ.txt -> txt
+            return fileName.substring(fileName.lastIndexOf(".")+1);
+            // в противном случае возвращаем заглушку, то есть расширение не найдено
+        else return " ";
     }
 
     public static class MyFileNameFilter implements FilenameFilter {
@@ -53,8 +102,7 @@ public class TheBypassFiles {
                 if(item.isDirectory()){
                     //System.out.println(item.getName() + "  \tкаталог");
                     folder(item);
-                }
-                else{
+                } else{
                     k++;
                     //System.out.println(item.getName() + "\tфайл");
                 }
